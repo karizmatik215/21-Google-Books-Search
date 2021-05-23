19bf4dcf60e8382655c8af9fe37ef6647c9891d2
Juan Sanchez <juan.sanchez@phila.gov>
2021-05-23 16:40:10 -0400

    changed book schema

--- models/book.js
+++ models/book.js
@@ -2,32 +2,26 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  id: {
+  _id: {
     type: String,
-    require: true,
   },
   title: {
     type: String,
-    require: true,
   },
   authors: {
     type: Array,
-    require: true,
   },
   description: {
     type: String,
-    require: true,
   },
   image: {
     type: String,
-    unique: true,
   },
   link: {
     type: String,
-    required: true,
   },
 });
 
-const GoogleBooks = mongoose.model('GoogleBooks', bookSchema);
+const Book = mongoose.model('Book', bookSchema);
 
-module.exports = GoogleBooks;
+module.exports = Book;
